cd0dd5502ba9a1e6493c9619e3e39fef61cbfd6a
Jiachen Sun <jcsun0806@gmail.com>
2022-02-09 00:31:27 -0500

    remove tf files

--- pointnet2_tf/part_seg/command.sh
+++
@@ -1,1 +0,0 @@
-python train.py --model pointnet2_part_seg --log_dir log --gpu 1 --max_epoch 201 > log.txt 2>&1 &
--- pointnet2_tf/part_seg/command_one_hot.sh
+++
@@ -1,1 +0,0 @@
-python train_one_hot.py --batch_size 8 --model pointnet2_part_seg_msg_one_hot --log_dir log_msg_one_hot --gpu 0 --max_epoch 201 > log_msg_one_hot.txt 2>&1 &
