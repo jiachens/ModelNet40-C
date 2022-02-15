c68e11f6b679511a9b8d3a922929843a96a598db
Jiachen Sun <jcsun0806@gmail.com>
2022-02-14 22:14:11 -0500

    fix clean evaluation script

--- eval_og.sh
+++ eval_og.sh
@@ -1,12 +1,11 @@
-
 if [ ! -d "output" ]; then
     mkdir "output"
 fi
 
 for model in 'dgcnn' 'rscnn' 'pct' 'pointnet' 'pointnet2'  'simpleview'; do
-for aug in 'pgd'; do
+# for aug in 'pgd'; do
 
-CUDA_VISIBLE_DEVICES=0 python main.py --entry test --model-path runs/${aug}_${model}_run_1/model_best_test.pth --exp-config configs/dgcnn_${model}_run_1.yaml --output ./output/${model}_${aug}_clean.txt
+CUDA_VISIBLE_DEVICES=0 python main.py --entry test --model-path cor_exp/dgcnn_${model}_run_1/model_best_test.pth --exp-config configs/dgcnn_${model}_run_1.yaml --output ./output/${model}_clean.txt
 
 done
 done
