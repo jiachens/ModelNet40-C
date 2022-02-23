44ebf713e8a7033de65f2f0a37a47772677faa93
Jiachen Sun <jcsun0806@gmail.com>
2022-02-23 16:54:34 -0500

    rm unnecessary code

--- pointnet2_pyt/pointnet2/utils/__init__.py
+++ pointnet2_pyt/pointnet2/utils/__init__.py
@@ -1,3 +1,10 @@
+'''
+Description: 
+Autor: Jiachen Sun
+Date: 2022-02-16 22:23:16
+LastEditors: Jiachen Sun
+LastEditTime: 2022-02-23 16:50:08
+'''
 from __future__ import (
     division,
     absolute_import,
@@ -5,5 +12,5 @@ from __future__ import (
     print_function,
     unicode_literals,
 )
-from . import pointnet2_utils
-from . import pointnet2_modules
+from PCT_Pytorch.pointnet2_ops_lib.pointnet2_ops import pointnet2_utils
+from PCT_Pytorch.pointnet2_ops_lib.pointnet2_ops import pointnet2_modules
