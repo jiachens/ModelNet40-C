e052f14b9489ec56b875650eb3cf514d41a3ae5a
Jiachen Sun <jcsun0806@gmail.com>
2022-02-23 15:39:18 -0500

    update readme and setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ PYTHON_BIN=${PYTHON_BIN:-python}
 
 abspath=$(readlink -f "$0")
 root=$(dirname $abspath)
-export TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5;8.6"
+export TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5"
 
 cd ${root}/pointnet2_pyt && ${PYTHON_BIN} -m pip install -e . && cd -
 cd ${root}/PCT_Pytorch/pointnet2_ops_lib && ${PYTHON_BIN} setup.py install && cd -
