5bb291e42445455972d90ab016da217dd5d4779a
Jiachen Sun <jcsun0806@gmail.com>
2022-02-24 23:20:21 -0500

    bug fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+###
+ # @Description: 
+ # @Autor: Jiachen Sun
+ # @Date: 2022-02-23 17:25:05
+ # @LastEditors: Jiachen Sun
+ # @LastEditTime: 2022-02-24 23:20:04
+### 
 set -e
 PYTHON_BIN=${PYTHON_BIN:-python}
 
@@ -5,7 +12,7 @@ abspath=$(readlink -f "$0")
 root=$(dirname $abspath)
 export TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5"
 
-# cd ${root}/pointnet2_pyt && ${PYTHON_BIN} -m pip install -e . && cd -
+cd ${root}/pointnet2_pyt && ${PYTHON_BIN} -m pip install -e . && cd -
 cd ${root}/PCT_Pytorch/pointnet2_ops_lib && ${PYTHON_BIN} setup.py install && cd -
 cd ${root}/emd && ${PYTHON_BIN} setup.py install && cd -
 cd ${root}/PyGeM && ${PYTHON_BIN} setup.py install && cd -
