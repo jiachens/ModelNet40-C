447c77257ad489c53efaf42e59695f14332eec07
Jiachen Sun <jcsun0806@gmail.com>
2023-04-03 00:21:02 -0400

    update newmodel after a while

--- eval_og.sh
+++ eval_og.sh
@@ -3,9 +3,9 @@ if [ ! -d "output" ]; then
     mkdir "output"
 fi
 
-for model in 'gdanet'; do #'dgcnn' 'rscnn' 'pct' 'pointnet' 'pointnet2'  'simpleview'  'curvenet' 'pointMLP';; do
+for model in 'robustnet'; do #'dgcnn' 'rscnn' 'pct' 'pointnet' 'pointnet2'  'simpleview'  'curvenet' 'pointMLP';; do
 # for aug in 'pgd'; do
 
-CUDA_VISIBLE_DEVICES=1 python main.py --entry test --model-path runs/dgcnn_${model}_run_1/model_best_test.pth --exp-config configs/dgcnn_${model}_run_1.yaml --output ./output/${model}_clean.txt
+CUDA_VISIBLE_DEVICES=3 python main.py --entry test --model-path runs/dgcnn_${model}_run_1/model_best_test.pth --exp-config configs/dgcnn_${model}_run_1.yaml --output ./output/${model}_clean.txt
 
 done
